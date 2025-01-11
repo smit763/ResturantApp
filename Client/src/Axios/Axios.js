4b59cc86b2b73d84933afb19a8d90f90b89a090f
smit763 <smitrakholiya763@gmail.com>
2025-01-11 09:07:58 +0530

    adding env files

--- Client/src/Axios/Axios.js
+++ Client/src/Axios/Axios.js
@@ -1,21 +1,6 @@
 import axios from "axios";
-import { url } from "../common";
 
 const axiosClient = axios.create({
-  baseURL: `${url}/api/v1`,
+  baseURL: `${process.env.REACT_APP_URI}/api/v1`,
 });
-
-axiosClient.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("token");
-    if (token) {
-      config.headers.token = token;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
-
 export default axiosClient;
